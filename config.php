2d032d568e0418d5ca4ada472127bb1f5a27f74a
kia4545 <81552960+kia4545@users.noreply.github.com>
2022-06-27 22:45:12 +0900

    設定ファイルの作成及び読み込み

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+//データベース接続情報
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'board');
+
+//タイムゾーン設定
+date_default_timezone_set('Asia/Tokyo');
--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-//データベース接続情報
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'board');
-
-//タイムゾーン設定
-date_default_timezone_set('Asia/Tokyo');
+//データベース設定を読み込み
+require_once 'config.php';
 
 //変数の初期化
 $current_date = null;
